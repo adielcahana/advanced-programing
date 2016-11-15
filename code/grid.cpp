6e4436c32c693f276d365680594a5e34a9db7729
adielcahana <adielcahana@users.noreply.github.com>
2016-11-15 04:01:11 +0200

    Add files via upload

---
+++ code/grid.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+class Point{
+	private:
+		int x;
+		int y;
+	public:		
+		int getX();
+		int getY();
+		bool operator ==(Point other);
+		int distance(Point other);
+		friend ostream &operator<<( ostream &output, Point p);
+};
+
+class Grid{
+	private:
+		int length;
+		int width;
+		vector<Point> obstacels;
+		queue<Point> get_adjacent(Point coordinate);		
+	public:		
+		queue<Point> get_route(Point start, Point end);
+		friend ostream &operator<<( ostream &output, Grid g);
+};
+
+
+
+//check what assgining obst does
+Grid::Grid(int length, int width, vector<Point> obstacels): length(length), width(width), obstacels(obstacels){};
+
+queue<Point> Grid::get_adjacent(Point coordinate){
+	queue<Point> que = new queue<Point>();
+	int x = point.getX();
+	int y = point.getY();
+	//if (x,y) not in obstacels
+	if (x - 1 > 0) que.push(new Point(x - 1, y)); 
+	if (x + 1 < width) que.push(new Point(x +1, y));
+	if (y + 1 < length) que.push(new Point(x , y + 1));
+	if (y - 1 > length) que.push(new Point(x , y + 1));
+	return queue;
+}
+
+queue<Point> Grid::get_route(Point start, Point end){
+	BFS bfs = new BFS();
+	return bfs.find_shortest_route(this, start, end);
+}
+
+friend ostream &operator<<( ostream &output, Grid g) { 
+	output << " ";
+	for (int i = 0; i <= g.length; i++) output << i;
+	output << endl << endl;		
+	for (int i = 0; i < g.length; i++){
+		output << i << " ";
+		for (int j = 0; j < g.width; j++){
+			/* if (new Point(i,j) in g.obstacles) {
+				output << "x";			
+				}*/
+			output << "-" << p.x << "," << p.y << ")";	
+		}
+		output << endl;		
+	}
+	return output;            
+}
+
+Point::Point(int x, int y): x(x), y(y) {};
+int Point::getX(){
+	return this.x;
+}
+
+Point::Point(int x, int y) x(x), y(y) {};
+
+int Point::getY(){
+	return this.y;
+}
+
+int Point::distance(Point other){
+	return abs(this.getX() - other.getX()) + abs(this.getY() - other.getY());
+}
+
+bool Point:: operator ==(Point other){
+	if((this.getX() == this.getY) && (this.getY() == other.getY())) return 1;
+	return 0;	
+}
+
+friend ostream &operator<<( ostream &output, Point p ) { 
+		output << "(" << p.x << "," << p.y << ")";
+		return output;            
+}
---
+++ code/grid_test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	std::vector<Point> v = new std::vector<Point>();
+	Grid g = new Grid(6,7,);
+	cout << g << endl;
+	Point p = new Point(3,4);
+	cout << p << endl;
+}
