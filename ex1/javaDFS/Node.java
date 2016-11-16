f4576508a87b31a66f9c85adae50a8a77cfd1e33
orie50 <turht50@gmail.com>
2016-11-16 21:20:15 +0200

    Add files via upload

---
+++ ex1/javaDFS/Node.java
@@ -0,0 +1,12 @@
+public class Node{
+		Point data;
+		Node left;
+		Node right;
+
+		public Node(Point data, Node left, Node right){
+			this.data = data;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
