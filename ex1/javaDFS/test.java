b242d5eb7cae7f72d0d8dc50ad65249d59ce9a57
orie50 <turht50@gmail.com>
2016-11-20 20:23:58 +0200

    Add files via upload

--- ex1/javaDFS/test.java
+++ ex1/javaDFS/test.java
@@ -1,8 +1,9 @@
 public class test {
 
 	public static void main(String[] args) {
-	BT bt = new BT(0);
-	bt.createBT(4096 * 64);
-	bt.DFSscan();
+		int leaves = Integer.parseInt(args[0]);
+		BT bt = new BT(0);
+		bt.createBT(leaves);
+		bt.DFSscan();
 	}
 }
