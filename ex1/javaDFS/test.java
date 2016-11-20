ac48c377fe75192f353dd52f8b99c867bed6bae1
orie50 <turht50@gmail.com>
2016-11-20 21:11:40 +0200

    Add files via upload

--- ex1/javaDFS/test.java
+++ ex1/javaDFS/test.java
@@ -4,6 +4,9 @@ public class test {
 		int leaves = Integer.parseInt(args[0]);
 		BT bt = new BT(0);
 		bt.createBT(leaves);
+		long startTime = System.currentTimeMillis();
 		bt.DFSscan();
+		long estimatedTime = System.currentTimeMillis() - startTime;
+		System.out.println("time passed is: " +(double) estimatedTime / 1000 + " seconds");
 	}
 }
