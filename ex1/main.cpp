0ef1a532a6a4d7b0b99ade7b261df41bd138ac6b
orie50 <turht50@gmail.com>
2016-11-23 21:16:29 +0200

    Add files via upload

---
+++ ex1/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+#include "Binary_tree.h"
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    if (argc <= 1) {
+        cout <<"not enough arguments" << endl;
+        return 0;
+    }
+    else {
+        Binary_tree *tree = new Binary_tree(0);
+        tree->create_full_tree(atoi(argv[1]));
+        clock_t start = clock();
+        tree->dfs();
+        clock_t end = clock();
+        cout.precision(5);
+        cout << "runtime: " << float( end - start ) /  CLOCKS_PER_SEC << " sec" << endl;
+        delete tree;
+        return 0;
+    }
+}
