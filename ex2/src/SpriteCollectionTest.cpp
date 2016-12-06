069f6bd2743c08b828037e38b292d36bff806628
adielcahana <adielcahana@users.noreply.github.com>
2016-12-06 03:43:16 +0200

    Delete SpriteCollectionTest.cpp

--- ex2/src/SpriteCollectionTest.cpp
+++
@@ -1,9 +0,0 @@
-/*#include "SpriteCollectionTest.h"
-
-TEST_F(SpriteCollectionTest, addSpriteClass){
-    Sprite sprite;
-    SpriteCollection sp;
-    sp.addSprite(sprite);
-    collection.addSprite(sprite);
-    EXPECT_EQ(sprite, sp.at(0));
-}*/
