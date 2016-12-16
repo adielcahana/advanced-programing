f40bcc3c6a233249eb60b290a3f013cdaab382a1
orie50 <turht50@gmail.com>
2016-12-16 11:11:04 +0200

    Add files via upload

--- ex3/tests/main.cpp
+++ ex3/tests/main.cpp
@@ -3,8 +3,8 @@
 //
 // Created by adi on 04/12/16.
 //
-int main(int argc, char **argv){
-    srand (time(NULL));
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+//int main(int argc, char **argv){
+//    srand (time(NULL));
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
